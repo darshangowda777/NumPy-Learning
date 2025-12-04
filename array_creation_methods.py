0cea99723d03e913968d808e3ebe13fa9ac127b5
darshangowda777 <darshanbm777@gmail.com>
2025-12-04 15:18:43 +0100

    add linspace and random array creation

--- array_creation_methods.py
+++ array_creation_methods.py
@@ -12,3 +12,41 @@ print(np.arange(0, 10))         # 0 to 9
 print(np.arange(1, 10, 2))      # odd numbers
 print(np.arange(10, 1, -2))     # reverse direction
 
+#3 np.linspace() — Generate exact number of values 
+print(np.linspace(0,1,5)) #generates 5 numbers between 0 and 1 with even spacing between numbers
+print(np.linspace(10,98,10))#generates 10 numbers between 10 and 98 with even spacing between numbers
+
+#4 np.zeros() — Create array filled with 0s (Useful for initialization)
+print(np.zeros(3))
+
+#multidimensional 
+print(np.zeros((4,7), ))
+print(np.zeros((3,5,8),dtype=int))
+
+#5 np.ones() — Create array filled with 1s (Useful for initialization)
+print(np.ones(3))
+print(np.ones((2,3), ))
+
+#6 Random Arrays — Important for ML & testing (NumPy random module helps generate arrays with random values)
+
+#Random float numbers between 0 and 1
+r1 = np.random.rand(3)
+print(r1)
+
+#Random numbers from normal distribution(mean=0 and SD=1)
+r2 = np.random.randn(5)
+print(r2)
+
+#Random integers
+#syntax:np.random.randint(low, high=None, size=None, dtype=int)
+r3 = np.random.randint(1,10,size=(5,5))
+print(r3)
+
+#Set random seed (important for reproducibility)
+'''Controls the randomness in NumPy
+Without a seed → every run gives different random numbers.
+With a seed → same random numbers every time'''
+
+#
+print(np.random.rand(3))
+
